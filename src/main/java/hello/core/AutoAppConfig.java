7ae043f1d97bffba28eeb9f6fc5c5e825147eb5f
smpark1020 <smpark911020@gmail.com>
2021-09-19 17:44:42 +0900

    [스프링 핵심 원리 - 기본편] 탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
